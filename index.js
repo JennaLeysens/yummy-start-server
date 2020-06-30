5dfe80c9fcf226431a2b588e624d918964890df3
JennaLeysens <jennalicowley@gmail.com>
2020-06-30 18:44:49 +0200

    edited index.js config.url (error fix)

--- index.js
+++ index.js
@@ -9,6 +9,6 @@ const cors = require("cors");
 app.use(cors());
 app.use(express.json());
 
-app.use("recipes", recipeRouter);
+app.use("/recipes", recipeRouter);
 
-app.listen(PORT, () => console.log("Listening on port"`${PORT}`));
+app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
