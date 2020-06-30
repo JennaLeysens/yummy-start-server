9512c21cee66cded6a1dafed74b1c3ef153b3b84
JennaLeysens <jennalicowley@gmail.com>
2020-06-30 16:00:09 +0200

    created recipe model

---
+++ models/recipe.js
@@ -0,0 +1,29 @@
+"use strict";
+const { Model } = require("sequelize");
+module.exports = (sequelize, DataTypes) => {
+  class recipe extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  recipe.init(
+    {
+      title: { type: DataTypes.STRING, allowNull: false },
+      imageURL: { type: DataTypes.STRING, allowNull: false },
+      description: { type: DataTypes.TEXT, allowNull: false },
+      ingredients: { type: DataTypes.TEXT, allowNull: false },
+      method: { type: DataTypes.TEXT, allowNull: false },
+      cookingTime: { type: DataTypes.STRING, allowNull: false },
+    },
+    {
+      sequelize,
+      modelName: "recipe",
+    }
+  );
+  return recipe;
+};
