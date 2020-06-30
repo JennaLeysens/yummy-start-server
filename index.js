4d2c3acda285825dd84e3f523e6d06963f7b203a
JennaLeysens <jennalicowley@gmail.com>
2020-06-30 18:25:40 +0200

    installed cors and added recipeRouter to index.js

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 require("dotenv").config();
+const express = require("express");
+const recipeRouter = require("./routers/recipes");
+
+const PORT = 4000;
+const app = express();
+const cors = require("cors");
+
+app.use(cors());
+app.use(express.json());
+
+app.use("recipes", recipeRouter);
+
+app.listen(PORT, () => console.log("Listening on port"`${PORT}`));
