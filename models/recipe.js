5bf2343354d5cc626f29d83b10a2320158161d99
JennaLeysens <jennalicowley@gmail.com>
2020-06-30 17:37:55 +0200

    seed data for users and recipes

--- models/recipe.js
+++ models/recipe.js
@@ -8,7 +8,7 @@ module.exports = (sequelize, DataTypes) => {
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
-      artwork.belongsTo(models.user);
+      recipe.belongsTo(models.user);
     }
   }
   recipe.init(
