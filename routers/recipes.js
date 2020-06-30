9a050d6d18849b020a9f7eef4b68d7fc9c41c55f
JennaLeysens <jennalicowley@gmail.com>
2020-06-30 18:15:44 +0200

    added recipes router with GET all recipes query

---
+++ routers/recipes.js
@@ -0,0 +1,13 @@
+const { Router } = require("express");
+const Recipe = require("../models").recipe;
+const router = new Router();
+
+router.get("/", async (req, res, next) => {
+  try {
+    const recipes = await Recipe.findAll();
+    res.json(recipes);
+  } catch (e) {
+    console.log(e.message);
+    next(e);
+  }
+});
