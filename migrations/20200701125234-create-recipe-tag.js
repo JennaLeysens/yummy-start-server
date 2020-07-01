ce8e7014daacb5d0609ee3b3f2372ff85b655320
JennaLeysens <jennalicowley@gmail.com>
2020-07-01 15:36:24 +0200

    set up many to many relationships for tags and recipeTags

--- migrations/20200701125234-create-recipe-tag.js
+++ migrations/20200701125234-create-recipe-tag.js
@@ -1,30 +1,42 @@
-'use strict';
+"use strict";
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    await queryInterface.createTable('recipeTags', {
+    await queryInterface.createTable("recipeTags", {
       id: {
         allowNull: false,
         autoIncrement: true,
         primaryKey: true,
-        type: Sequelize.INTEGER
+        type: Sequelize.INTEGER,
       },
       recipeId: {
-        type: Sequelize.INTEGER
+        type: Sequelize.INTEGER,
+        references: {
+          model: "recipes",
+          key: "id",
+        },
+        onUpdate: "CASCADE",
+        onDelete: "CASCADE",
       },
       tagId: {
-        type: Sequelize.INTEGER
+        type: Sequelize.INTEGER,
+        references: {
+          model: "tags",
+          key: "id",
+        },
+        onUpdate: "CASCADE",
+        onDelete: "CASCADE",
       },
       createdAt: {
         allowNull: false,
-        type: Sequelize.DATE
+        type: Sequelize.DATE,
       },
       updatedAt: {
         allowNull: false,
-        type: Sequelize.DATE
-      }
+        type: Sequelize.DATE,
+      },
     });
   },
   down: async (queryInterface, Sequelize) => {
-    await queryInterface.dropTable('recipeTags');
-  }
+    await queryInterface.dropTable("recipeTags");
+  },
 };
--- models/recipetag.js
+++ models/recipetag.js
@@ -1,7 +1,5 @@
-'use strict';
-const {
-  Model
-} = require('sequelize');
+"use strict";
+const { Model } = require("sequelize");
 module.exports = (sequelize, DataTypes) => {
   class recipeTag extends Model {
     /**
@@ -10,15 +8,19 @@ module.exports = (sequelize, DataTypes) => {
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
-      // define association here
+      recipeTag.belongsTo(models.tag);
+      recipeTag.belongsTo(models.recipe);
     }
-  };
-  recipeTag.init({
-    recipeId: DataTypes.INTEGER,
-    tagId: DataTypes.INTEGER
-  }, {
-    sequelize,
-    modelName: 'recipeTag',
-  });
+  }
+  recipeTag.init(
+    {
+      recipeId: DataTypes.INTEGER,
+      tagId: DataTypes.INTEGER,
+    },
+    {
+      sequelize,
+      modelName: "recipeTag",
+    }
+  );
   return recipeTag;
 };
