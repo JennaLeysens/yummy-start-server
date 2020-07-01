f11598f1602cb55cd38ca40015dd063487a840d9
JennaLeysens <jennalicowley@gmail.com>
2020-07-01 10:22:07 +0200

    included User model in all recipes query

--- routers/recipes.js
+++ routers/recipes.js
@@ -1,10 +1,13 @@
 const { Router } = require("express");
 const Recipe = require("../models").recipe;
+const User = require("../models").user;
 const router = new Router();
 
 router.get("/", async (req, res, next) => {
   try {
-    const recipes = await Recipe.findAll();
+    const recipes = await Recipe.findAll({
+      include: User,
+    });
     res.json(recipes);
   } catch (e) {
     console.log(e.message);
