27a70799dc835cb1a3ed6baf1e834c97ef6eb3e4
JennaLeysens <jennalicowley@gmail.com>
2020-07-01 15:41:38 +0200

    seed file and seed data for tag model

---
+++ seeders/20200701133756-tags.js
@@ -0,0 +1,33 @@
+"use strict";
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    return queryInterface.bulkInsert("tags", [
+      { title: "Vegan", createdAt: new Date(), updatedAt: new Date() },
+      {
+        title: "Vegetarian",
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        title: "Breakfast",
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        title: "Lunch",
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        title: "Dinner",
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+    ]);
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    return queryInterface.bulkDelete("tags", null, {});
+  },
+};
