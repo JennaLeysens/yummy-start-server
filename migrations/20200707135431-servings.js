7046f54d02dffa9f5f24697c27e94ca3c9888660
JennaLeysens <jennalicowley@gmail.com>
2020-07-08 12:23:18 +0200

    added a servings column to recipe model

---
+++ migrations/20200707135431-servings.js
@@ -0,0 +1,13 @@
+"use strict";
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    return Promise.all([
+      queryInterface.addColumn("recipes", "servings", Sequelize.STRING),
+    ]);
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    return Promise.all([queryInterface.removeColumn("recipes", "servings")]);
+  },
+};
