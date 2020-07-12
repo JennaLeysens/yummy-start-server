e366fe57a1089f483b253859856f751aa50ed464
JennaLeysens <jennalicowley@gmail.com>
2020-07-12 16:27:34 +0200

    updated recipe endpoint to include user's recipes

--- routers/auth.js
+++ routers/auth.js
@@ -22,9 +22,9 @@ router.post("/login", async (req, res, next) => {
 
     const user = await User.findOne({
       where: { email },
-      include: [{ model: Recipe }, { model: Favourite, as: "userFavourites" }],
+      include: [{ model: Favourite, as: "userFavourites" }],
     });
-
+    const recipes = await Recipe.findAll({ where: { userId: user.id } });
     if (!user || !bcrypt.compareSync(password, user.password)) {
       return res.status(400).send({
         message: "Incorrect combination of email address and password",
@@ -33,7 +33,7 @@ router.post("/login", async (req, res, next) => {
 
     delete user.dataValues["password"];
     const token = toJWT({ userId: user.id });
-    return res.status(200).send({ token, ...user.dataValues });
+    return res.status(200).send({ token, ...user.dataValues, recipes });
   } catch (error) {
     console.log(error);
     return res.status(400).send({ message: "Something went wrong, sorry" });
@@ -166,7 +166,7 @@ router.delete("/deleterecipe/:id", authMiddleware, async (req, res) => {
     console.log(recipe);
     if (user.id === recipe.userId) {
       const deletedRecipe = await recipe.destroy();
-      res.status(201).send({ message: "Recipe deleted", deletedRecipe });
+      res.status(201).send({ message: "Recipe deleted", recipe });
     } else {
       return res
         .status(400)
